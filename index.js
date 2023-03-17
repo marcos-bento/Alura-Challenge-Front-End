02556c102e02ea405c922e33fa35a52e0b8309bf
Marcos Bento <marcos.x.marcos@hotmail.com>
2023-03-16 23:21:17 -0300

    Finalização da tela informações.html

--- index.js
+++ index.js
@@ -5,3 +5,20 @@ botaoMenu.addEventListener('click', () =>{
 	menuHamburguer.classList.toggle('menu__hamburguer__exibido')
 	botaoMenu.classList.toggle('cabecalho__menu__botao__on')
 })
+
+// Trecho que controla o Accordion em informacoes.html
+
+var acc = document.getElementsByClassName("principal__conteudo__container__accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("principal__conteudo__container__accordion-active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
